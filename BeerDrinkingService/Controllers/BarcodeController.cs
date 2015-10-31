ae4d8032dea50531fa905c321950b4067e2a80f3
Mike James <mike@xamarin.com>
2015-10-31 19:14:39 +0000

    Added barcode controller

---
+++ BeerDrinkingService/Controllers/BarcodeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service.Security;
+using BeerDrinkin.Service.Models;
+using BeerDrinkin.Service.DataObjects;
+
+namespace BeerDrinkin.Service.Controllers
+{
+    [AuthorizeLevel(AuthorizationLevel.User)]
+    public class BarcodeController : ApiController
+    {
+        public ApiServices Services { get; set; }
+
+        // GET api/beer
+        public async Task<List<BeerItem>> Get(string upc)
+        {
+            BeerDrinkinContext context = new BeerDrinkinContext();
+            var beers = context.BeerItems.Where(x => x.UPC == upc).ToList();
+
+            return beers;
+        }
+
+    }
+}
