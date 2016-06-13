6d0e777a07152bba688176b7a6749bbd2e6e2f23
Mike James <mike@xamarin.com>
2016-06-13 11:59:43 +0100

    Adding telemetry to user controller

--- Azure/BeerDrinkinService/Controllers/UserController.cs
+++ Azure/BeerDrinkinService/Controllers/UserController.cs
@@ -6,16 +6,26 @@ using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using BeerDrinkin.DataObjects;
 using BeerDrinkin.Models;
+using Microsoft.ApplicationInsights;
 
 namespace BeerDrinkin.Controllers
 {
     public class UserController : TableController<User>
     {
+        TelemetryClient telemetryClient;
         protected override void Initialize(HttpControllerContext controllerContext)
         {
             base.Initialize(controllerContext);
             BeerDrinkinContext context = new BeerDrinkinContext();
             DomainManager = new EntityDomainManager<User>(context, Request);
+            telemetryClient = new TelemetryClient();
+        }
+
+        // GET tables/User
+        public IQueryable<User> GetAllUser()
+        {
+            telemetryClient.TrackEvent("GetAllUsers");
+            return Query();
         }
 
         // GET tables/User/48D68C86-6EA6-4C25-AA33-223FC9A27959
