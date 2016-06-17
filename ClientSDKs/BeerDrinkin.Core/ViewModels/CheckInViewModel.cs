e561c72780256c75bdba09d2672bde013fc24c99
Mike James <mike@xamarin.com>
2016-06-17 10:04:47 +0100

    Adding checkin view model

---
+++ ClientSDKs/BeerDrinkin.Core/ViewModels/CheckInViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerDrinkin.AzureClient;
+using BeerDrinkin.DataObjects;
+using BeerDrinkin.DataStore.Abstractions;
+using BeerDrinkin.Services.Abstractions;
+using BeerDrinkin.Utils;
+using Plugin.Geolocator;
+
+namespace BeerDrinkin.Core.ViewModels
+{
+    public class CheckInViewModel : ViewModelBase
+    {
+        ICheckInStore checkInStore;
+        IAzureClient azure;
+        ILogService log;
+
+        public CheckInViewModel(Beer beer)
+        {
+            checkInStore = ServiceLocator.Instance.Resolve<ICheckInStore>();
+            azure = ServiceLocator.Instance.Resolve<IAzureClient>();
+            log = ServiceLocator.Instance.Resolve<ILogService>();
+
+            BeerName = beer.Name;
+            ABV = beer.Abv;
+        }
+
+        public async Task CheckInBeer(Beer beer, Rating rating, bool isBottled)
+        {
+            try
+            {
+                var location = await CrossGeolocator.Current.GetPositionAsync();
+                var checkIn = new CheckIn
+                {
+                    BeerId = beer.Id,
+                    UserId = azure.Client.CurrentUser.UserId,
+                    Rating = rating,
+                    IsBottled = isBottled,
+                    Longitude = location.Longitude,
+                    Latitude = location.Latitude
+                };
+
+                await checkInStore.InsertAsync(checkIn);
+            }
+            catch (Exception ex)
+            {
+                log.Report(ex);
+            }
+        }
+
+        public string BeerName { get; set;}
+        public double? ABV { get; set;}
+
+    }
+}
+
