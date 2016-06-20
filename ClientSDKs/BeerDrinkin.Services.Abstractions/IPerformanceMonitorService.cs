be87b02c035e30140a4739a2c4840f40a6ee7a8a
Mike James <mike@xamarin.com>
2016-06-20 13:12:39 +0200

    Git really does like to moan...

---
+++ ClientSDKs/BeerDrinkin.Services.Abstractions/IPerformanceMonitorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace BeerDrinkin.Services.Abstractions
+{
+    public interface IPerformanceMonitorService
+    {
+        void Track(string eventName);
+
+        void Track(string eventName, IDictionary details);
+
+        void Track(string eventName, TimeSpan time);
+    }
+}
+
